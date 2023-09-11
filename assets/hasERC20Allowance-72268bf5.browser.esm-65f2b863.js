eb196ee92c5d10c0590ce692293aefa853075497
GreatApe78 <GreatApe78@users.noreply.github.com>
2023-09-11 21:17:41 +0000

    deploy: e88ebb017189180998c01661674b35aee587dfaa

---
+++ assets/hasERC20Allowance-72268bf5.browser.esm-65f2b863.js
@@ -0,0 +1,1 @@
+import{t as d,aL as i}from"./index-463be5f5.js";async function c(a,e,s){const n=a.getProvider(),r=new d(n,e,i,{},a.storage),t=await a.getSignerAddress(),o=a.address;return(await r.read("allowance",[t,o])).gte(s)}export{c as h};
